807d81cb1d12155b25e46192db0d443224ed0b02
Evan Pierre Louis <evan.dev@yandex.com>
2022-01-18 15:10:06 +0100

    Fix minor import issue

--- libs/e-commerce-data/src/index.ts
+++ libs/e-commerce-data/src/index.ts
@@ -1,2 +1,3 @@
 export * from './lib/e-commerce-data.module';
+
 export * from './lib/application/e-commerce.facade';
--- libs/e-commerce-data/src/lib/e-commerce-data.module.ts
+++ libs/e-commerce-data/src/lib/e-commerce-data.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { ECommerceFacade } from '..';
+import { ECommerceFacade } from './application/e-commerce.facade';
 
 @NgModule({
   imports: [CommonModule],
