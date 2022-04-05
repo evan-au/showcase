cae348db6253e1907a6dc64a1174e0397c073d24
Evan Pierre Louis <evan.pierrelouis.dev@gmail.com>
2022-04-06 01:37:48 +0200

    configure firebase

--- apps/showcase-app/src/environments/environment.prod.ts
+++ apps/showcase-app/src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
   production: true,
+  firebase: {
+    apiKey: 'AIzaSyBPIxuWBRlD9goZ-9IXLmpQVcLx72hzAS8',
+    authDomain: 'expo-app-evan-pl-angular-dev.firebaseapp.com',
+    projectId: 'expo-app-evan-pl-angular-dev',
+    storageBucket: 'expo-app-evan-pl-angular-dev.appspot.com',
+    messagingSenderId: '431498096147',
+    appId: '1:431498096147:web:a55c74161ca853ef1f59e6',
+  },
 };
--- apps/showcase-app/src/environments/environment.ts
+++ apps/showcase-app/src/environments/environment.ts
@@ -4,6 +4,14 @@
 
 export const environment = {
   production: false,
+  firebase: {
+    apiKey: 'AIzaSyBPIxuWBRlD9goZ-9IXLmpQVcLx72hzAS8',
+    authDomain: 'expo-app-evan-pl-angular-dev.firebaseapp.com',
+    projectId: 'expo-app-evan-pl-angular-dev',
+    storageBucket: 'expo-app-evan-pl-angular-dev.appspot.com',
+    messagingSenderId: '431498096147',
+    appId: '1:431498096147:web:a55c74161ca853ef1f59e6',
+  },
 };
 
 /*
